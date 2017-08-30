b56c9c34870bcbc6597f68a5d5843715ca4b9b39
Jacky1100 <609615907@qq.com>
2017-08-30 18:13:53 +0800

    Jack update

--- src/main/java/com/paipianwang/pat/facade/finance/service/PmsFinanceFacade.java
+++ src/main/java/com/paipianwang/pat/facade/finance/service/PmsFinanceFacade.java
@@ -1,5 +1,6 @@
 package com.paipianwang.pat.facade.finance.service;
 
+import java.util.List;
 import java.util.Map;
 
 import com.paipianwang.pat.common.entity.DataGrid;
@@ -23,4 +24,11 @@ public interface PmsFinanceFacade {
 	 */
 	public boolean isExistByProjectId(final String projectId);
 	
+	/**
+	 * 根据projectId获取财务数据
+	 * @param metaData 元数据
+	 * @param projectId 项目ID
+	 * @return
+	 */
+	public Map<String, List<Map<String, Object>>> getFinancesByProjectId(List<String> metaData, String projectId);
 }
